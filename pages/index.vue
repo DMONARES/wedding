316fd3352f13c91d4d17b4394da1a43927eefcd9
David <dmonaresss@gmail.com>
2025-06-11 12:37:34 +0300

    First commit

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<script setup>
+	useSeoMeta({
+		title      : 'NUXT-TEMPLATE',
+		description: 'NUXT-TEMPLATE - description',
+		keywords   : 'NUXT-TEMPLATE - keywords',
+	});
+</script>
+
+<template>
+	<div>
+	</div>
+</template>
+
+<style lang='scss'>
+
+</style>
