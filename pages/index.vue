d2593c9dd8e88564f27ccf5a34128f0f03f066a7
David <dmonaresss@gmail.com>
2025-06-15 02:08:14 +0300

    Test autodeploy

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,7 @@
 
 <template>
 	<div>
+		<h1>Тест автовыката</h1>
 		<!-- <YandexMap style="margin-top: 30px" /> -->
 		<MyForm style="margin-top: 30px; margin-bottom: 30px;" />
 		<!-- <Timer /> -->
