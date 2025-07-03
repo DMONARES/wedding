a72048e4a261fddc248a1ac0eca52a825d326ec3
Dmonarez <dmonaresss@gmail.com>
2025-07-03 17:35:49 +0300

    Fix redirect

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -2,7 +2,7 @@ import { useAuth } from "@/composables/useAuth";
 import { defineNuxtRouteMiddleware, navigateTo } from "#app";
 
 export default defineNuxtRouteMiddleware((to, from) => {
-	if (to.path === "/guests") {
+	if (to.path === "/guests" || to.path === "/guests/") {
 		const { isAuth } = useAuth();
 		if (!isAuth.value) {
 			return navigateTo("/login");
